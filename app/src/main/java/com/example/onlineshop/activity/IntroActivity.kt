897cfa44c298f7540b68b2854732374e1dc01df6
André Morientes <59062594+andremorientes@users.noreply.github.com>
2025-01-13 22:21:22 +0200

    Implementando a IntroActivity

---
+++ app/src/main/java/com/example/onlineshop/activity/IntroActivity.kt
@@ -0,0 +1,28 @@
+package com.example.onlineshop.activity
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.onlineshop.MainActivity
+import com.example.onlineshop.R
+import com.example.onlineshop.databinding.ActivityIntroBinding
+
+    class IntroActivity : BaseActivity() {
+    private lateinit var binding: ActivityIntroBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivityIntroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnStart.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+
+    }
+
+}
